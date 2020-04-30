7b42f853d7054a2dbd6942185cc06b5c3498e6ab
kz56cd <kz56cd@gmail.com>
2020-04-30 11:48:28 +0900

    Tweak UI and clean analyze logic

--- ImageDiscriminator/ContentView.swift
+++ ImageDiscriminator/ContentView.swift
@@ -11,38 +11,61 @@ import Firebase
 
 struct ContentView: View {
     @State var isShowAlert = false
-    @State var isAnalyzed = false
+    @State var isAnalyzing = false
     @State var resultString = ""
     
     var body: some View {
         Group<AnyView> {
             guard let rawImage = UIImage(named: "cat01") else { return AnyView(Text("ImageDescripter")) }
-            self.analyze(for: rawImage)
             return AnyView(
-                VStack(alignment: .center, spacing: 20.0) {
+                VStack(
+                    alignment: .center,
+                    spacing: 40.0
+                ) {
                     Spacer()
-                    Text("ImageDescripter")
+                    Text("ImageDiscriminator")
+                        .font(.title)
                     Image(uiImage: rawImage)
-                        .border(Color.gray, width: 2)
+                        .border(
+                            Color.gray,
+                            width: 2
+                        )
+                    Button("analyze") { self.analyze(for: rawImage) }
+                        .padding()
+                        .background(Color.yellow)
+                        .foregroundColor(.purple)
+                        .border(
+                            Color.purple,
+                            width: 5
+                        )
+                        .font(.headline)
                     Spacer()
                 }
             )
         }
         .background(Color.yellow)
+        .edgesIgnoringSafeArea(.top)
+        .edgesIgnoringSafeArea(.bottom)
         .alert(isPresented: $isShowAlert) { () -> Alert in
             Alert(
                 title: Text("Result"),
-                message: Text(resultString)
+                message: Text(resultString),
+                dismissButton: .default(
+                    Text("OK"),
+                    action: {
+                        self.change(alertState: .reset)
+                        print("resultString: \(self.resultString)")
+                    }
+                )
             )
-        }.onDisappear {
-            self.isShowAlert = false
         }
     }
 }
 
 extension ContentView {
     private func analyze(for image: UIImage) {
-        guard !isAnalyzed else { return }
+        guard !isAnalyzing else { return }
+        change(alertState: .progress)
         print("🔍 analyze: start")
         
         // ↓ CMSampleBufferRef型で利用したい場合は別処理が必要
@@ -52,18 +75,39 @@ extension ContentView {
         // let labeler = Vision.vision().onDeviceImageLabeler() // ローカルモデルを利用する場合
         let labeler = Vision.vision().cloudImageLabeler() // クラウドモデルを利用する場合
         labeler.process(visionImage) { labels, error in
-            self.isAnalyzed = true
             guard error == nil,
                 let labels = labels else {
                     print("ERROR: \(error ?? "unknown." as! Error)")
+                    self.change(alertState: .reset)
                 return
             }
             labels.forEach { label in
                 self.resultString += "\(label.text)" + "\n"
             }
-            self.isShowAlert = true
+            self.change(alertState: .show)
             print("🔍 analyze: end")
         }
+    }
+    
+    private func change(alertState: AlertState) {
+        switch alertState {
+        case .reset:
+            isAnalyzing = false
+            isShowAlert = false
+            resultString = ""
+        case .show:
+            isAnalyzing = false
+            isShowAlert = true
+        case .progress:
+            isAnalyzing = true
+            isShowAlert = false
+        }
+    }
+    
+    private enum AlertState {
+        case reset
+        case show
+        case progress
     }
 }
 
